6ccd707f92fc671592e69d1b4ed76ce02858be35
na4ma4 <na4ma4@users.noreply.github.com>
2026-02-26 17:49:03 +1000

    fix: linter warning in non-darwin code

--- internal/codec/default_other.go
+++ internal/codec/default_other.go
@@ -2,4 +2,7 @@
 
 package codec
 
+// Default is a no-op codec that does not perform any encoding or decoding.
+//
+//nolint:gochecknoglobals // Default is intended to be a global variable for ease of use across the application.
 var Default Codec = NewNoop()
